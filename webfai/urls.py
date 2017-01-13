b7ff0c04a0a94b86fb7548d27d9ba0edda94572d
AFred38 <frederic.audra@gmail.com>
2017-01-13 13:44:31 +0100

    Reprise...

--- webfai/urls.py
+++ webfai/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url,patterns,include
+#from django.conf.urls import url,patterns,include
+from django.conf.urls import url,include
 
 from . import views
 #app_name = 'webfai'
